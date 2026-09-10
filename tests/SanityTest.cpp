4927ae6f0d5a71df1fa1860e877a53a6888a572d
h3rogam3r8 <arnavsawant@gmail.com>
2026-09-10 17:54:26 -0400

    Got CMake set up again, Using GoogleTest because it works seamlessly. No compilet yet obv

---
+++ tests/SanityTest.cpp
@@ -0,0 +1,8 @@
+// Placeholder so ctest has something to run before the lexer exists.
+// Deleting this once there are real tests.
+
+#include <gtest/gtest.h>
+
+TEST(Sanity, CheckAdditionWorks) {
+  EXPECT_EQ(2 + 2, 4);
+}
---
+++ tools/heroc/main.cpp
@@ -0,0 +1,8 @@
+// The compiler driver. Doesn't do anything yet.
+
+#include <cstdio>
+
+int main() {
+  std::puts("heroc: no compiler here yet");
+  return 0;
+}
